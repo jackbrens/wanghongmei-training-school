d7cb182a56ce4fca65ea8bd71eea12d0aa2b3a20
JackBrens <574950259@qq.com>
2024-05-10 21:19:52 +0800

    修改base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/wanghongmei-training-school/',
   plugins: [
     vue(),
     vueJsx(),
